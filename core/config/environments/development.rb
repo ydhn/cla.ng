4885d6cc4ac821fc2a6de6a4c52a3c56bec62cad
Yundo Han <han@yun.do>
2018-05-21 04:07:27 +0900

    set whiny request false

--- core/config/environments/development.rb
+++ core/config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.web_console.whiny_requests = false
   # Don't care if the mailer can't send.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = false
