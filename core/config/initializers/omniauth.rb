abf2a5827e6f40d2142da28de4409777dad48839
Yundo Han <han@yun.do>
2018-05-21 08:03:35 +0900

    change omniauth setting

--- core/config/initializers/omniauth.rb
+++ core/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :kakao, ENV['KAKAO_API_KEY'], {:redirect_path => '/users/auth/kakao/callback'}
+  provider :kakao, ENV['KAKAO_API_KEY'], {:redirect_path => '/users/auth/kakao/callback', :provider_ignores_state => true}
 end
